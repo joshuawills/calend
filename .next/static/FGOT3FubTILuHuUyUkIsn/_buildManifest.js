850d410593b3cd884f6ac33ee5c98dfb68c2eca7
joshuawills <joshuamatthewwills@gmail.com>
2023-04-22 20:59:55 +1000

    Adding in .next directory

---
+++ .next/static/FGOT3FubTILuHuUyUkIsn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/08b5101ce004cc14.css","static/chunks/pages/index-34221a7e44d9e708.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
